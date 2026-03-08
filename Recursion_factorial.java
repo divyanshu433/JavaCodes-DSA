a45cab0c692346e679ab4f4c888477811e54b8b0
divyanshu433 <work.divyanshugupta@gmail.com>
2026-03-09 00:19:55 +0530

    recursion

---
+++ Recursion_factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Recursion_factorial {
+
+    public static int fact (int n){
+          if ((n == 0) || (n == 1)) {
+            return 1;
+          }
+          return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number to calc : ");
+        int num = sc.nextInt();
+
+        System.out.println("Factorial is : " + fact(num));
+
+        sc.close();
+    }
+}
