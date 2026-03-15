72e49d4152d26f3b11fae69a677264caccc31253
divyanshu433 <work.divyanshugupta@gmail.com>
2026-03-15 17:12:24 +0530

    Improved eff

--- Recursion_15_numbers_to_words.java
+++ Recursion_15_numbers_to_words.java
@@ -1,18 +1,22 @@
 public class Recursion_15_numbers_to_words {
 
-    public static void numbers_to_words (int num, String str){
+    public static void numbers_to_words (int num, StringBuilder str){
         if (num == 0){
-            System.out.println(str);
             return;
         }
+
         int lst = num % 10;
-        num = num / 10;
+
         String arr[] = {"zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine "};
 
-        numbers_to_words(num, arr[lst] + str);
+        numbers_to_words(num/10, str);
+        
+        str.append(arr[lst]);
     }
 
     public static void main(String[] args) {
-        numbers_to_words(1947, "");
+        StringBuilder sb = new StringBuilder();
+        numbers_to_words(1947, sb);
+        System.out.println(sb);
     }
 }
