fed9f061ffa04b4afa9f7253138786fd499d8572
divyanshu433 <work.divyanshugupta@gmail.com>
2026-03-21 23:09:01 +0530

    merge sort

---
+++ Divide_and_conquer_01_merge_sort.java
@@ -0,0 +1,18 @@
+public class Divide_and_conquer_01_merge_sort {
+
+    public static void print_Arr (int arr[]){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void merge_sort (int arr[]){
+        
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {6, 3, 9, 5, 2, 8};
+    }
+}
