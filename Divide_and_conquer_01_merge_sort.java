c440e61b8c372a6f6f0e4a8004a857c8bc140bd1
divyanshu433 <work.divyanshugupta@gmail.com>
2026-03-22 18:48:10 +0530

    merge sort

--- Divide_and_conquer_01_merge_sort.java
+++ Divide_and_conquer_01_merge_sort.java
@@ -7,12 +7,53 @@ public class Divide_and_conquer_01_merge_sort {
         System.out.println();
     }
 
-    public static void merge_sort (int arr[]){
+    public static void merge_sort (int arr[], int si, int ei){
+        if (si >= ei){
+            return;
+        }
+
+        int mid = si + (ei - si) / 2;
+        merge_sort(arr, si, mid);
+        merge_sort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
         
+    }
+
+    public static void merge (int arr[], int si, int mid, int ei){
+        int temp[] = new int[ei - si + 1];
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while ((i <= mid) && (j <= ei)){
+            if (arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+                k++;
+            }
+            else{
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+        }
+        while (i <= mid){
+            temp[k++] = arr[i++];
+        }
+        while (j <= ei){
+            temp[k++] = arr[j++];
+        }
         
+        for (k = 0, i = si; k < temp.length; k++, i++){
+            arr[i] = temp[k];
+        }
     }
 
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 5, 2, 8};
+
+        merge_sort(arr, 0, arr.length - 1);
+
+        print_Arr(arr);
     }
 }
