e337ab4d6833d188ad39fa4181fa43dd79076828
divyanshu433 <work.divyanshugupta@gmail.com>
2026-03-28 21:28:58 +0530

    Divide and conquer

--- Divide_and_conquer_04_rotated_sorted_arr2.java
+++ Divide_and_conquer_04_rotated_sorted_arr2.java
@@ -1,10 +1,40 @@
 public class Divide_and_conquer_04_rotated_sorted_arr2 {
 
-    public static void rotated_sorted_arr (int arr[], int key){
-        
+    public static int rotated_sorted_arr (int arr[], int key){
+        int si = 0;
+        int ei = arr.length - 1;
+
+
+        while (si <= ei){
+            int mid = (si + ei)/2;
+
+            if (arr[mid] == key){
+                return mid;
+            }
+
+            else if (arr[si] <= arr[mid]){
+                if ((arr[si] <= key) && (arr[mid] > key)){
+                    ei = mid - 1;
+                }
+                else{
+                    si = mid + 1;
+                }
+            }
+
+            else{
+                if ((arr[mid] < key) && (arr[ei] >= key)){
+                    si = mid + 1;
+                }
+                else{
+                    ei = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        
+        int arr[] = {5 ,6 ,7, 1, 2, 3, 4};
+        System.out.println("Found at idx : " + rotated_sorted_arr(arr, 3));
     }
 }
---
+++ Divide_and_conquer_05_merge_Sort_Str.java
@@ -0,0 +1,20 @@
+public class Divide_and_conquer_05_merge_Sort_Str {
+
+    public static void merge_Sort_Str (String arr[]){
+        int si = 0;
+        int ei = arr.length - 1;
+
+        while (si <= ei){
+            int mid = (si + ei) / 2;
+
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String arr[] = { "sun", "earth", "mars", "mercury"};
+
+        merge_Sort_Str(arr);
+    }
+}
