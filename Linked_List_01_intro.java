c938bebe0bbc1b5fe84a4e66e5fb126f02d8c021
divyanshu433 <work.divyanshugupta@gmail.com>
2026-06-07 15:40:52 +0530

    Linked list

--- Linked_List_01_intro.java
+++ Linked_List_01_intro.java
@@ -4,7 +4,7 @@ public class Linked_list_01_intro {
         int data;
         Node next;
 
-        public Node(){
+        public Node(int data){
             this.data = data;
             this.next = null;
         }
@@ -13,6 +13,7 @@ public class Linked_list_01_intro {
     public void addFirst(int data){
         // Step - 1 (Create new Node)
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -24,14 +25,255 @@ public class Linked_list_01_intro {
     }
 
     public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
         }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printLl(){
+        if(head == null){
+            System.out.println("Linked List is Empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void addInMid(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        int i = 0;
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        while(i < idx - 1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("Linked list is EMPTY");
+            return Integer.MIN_VALUE;
+        }
+        else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removelast(){
+        if(size == 0){
+            System.out.println("Linked List is EMPTY");
+            return Integer.MIN_VALUE;
+        }
+        else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        for(int i = 0; i < size - 2; i++){
+            prev = prev.next;
+        }
+        int val = tail.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int searchLl(int key){
+        Node temp = head;
+        for(int i = 0; i < size; i++){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+        }
+        return -1;
+    }
+
+    public int iteSearch(int key){
+        Node temp = head;
+        int i = 0;
+        while(temp != null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    public int helper(Node head, int key){
+        if(head == null){
+            return -1;
+        }
+
+        if(head.data == key){
+            return 0;
+        }
+
+        int idx = helper(head.next, key);
+
+        if(idx == -1){
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key){
+        return helper(head, key);
+    }
+
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void romove_from_end(int idx){
+        if(idx == size){
+            head = head.next;
+            size--;
+            return;
+        }
+        Node temp = head;
+        int i = 1;
+
+        while(i < size - idx){
+            temp = temp.next;
+            i++;
+        }
+
+        Node delete = temp.next;
+        temp.next = delete.next;
+        size--;
+        return;
+    }
+
+    // Slow Fast Approach 
+    public Node findMid(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;  // 1 step at a time
+            fast = fast.next.next;  // 2 step at a time
+        }
+        return slow;  // mid node
+    }
+
+    public boolean palindrome(){
+        if(head == null || head.next == null){
+            return true;
+        }
+        Node midNode = findMid(head);
+
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+
+        while(right != null){
+            if(right.data != left.data){
+                return false;
+            }
+            right = right.next;
+            left = left.next;
+        }
+        return true;
+    }
+
 
     public static Node head;
     public static Node tail;
-
+    public static int size;
     public static void main(String[] args) {
         Linked_list_01_intro ll = new Linked_list_01_intro();
+        ll.printLl();
         ll.addFirst(1);
+        ll.printLl();
         ll.addFirst(2);
+        ll.printLl();
+        ll.addLast(3);
+        ll.printLl();
+        ll.addLast(4);
+        ll.printLl();
+        ll.addInMid(2, 10);
+        ll.printLl();
+        System.out.println(ll.size);
+        ll.removeFirst();
+        ll.printLl();
+        System.out.println(ll.size);
+
+        ll.removelast();
+        ll.printLl();
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+        System.out.println(ll.searchLl(3));
+        System.out.println(ll.recSearch(3));
+        ll.printLl();
+        ll.reverse();
+        ll.printLl();
+        ll.reverse();
+        ll.printLl();
+        ll.romove_from_end(4);
+        ll.printLl();
+
+        ll.romove_from_end(4);
+        ll.printLl();
+        ll.removelast();
+        ll.removelast();
+        ll.removelast();
+        ll.removelast();
+        ll.printLl();
+
+        ll.addFirst(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.printLl();
+        System.out.println(ll.palindrome());
     }
 }
