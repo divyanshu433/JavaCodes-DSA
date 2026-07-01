aa0bfa7198cd2879ed7cafefb76cf394988159da
divyanshu433 <work.divyanshugupta@gmail.com>
2026-07-01 21:14:39 +0530

    BST

--- BST_01.java
+++ BST_01.java
@@ -136,8 +136,8 @@ public class BST_01 {
         return root;
     }
 
-    public static void preorder(Node root){
-        if(root == null){
+    public static void preorder(Node root) {
+        if (root == null) {
             return;
         }
         System.out.print(root.data + " ");
@@ -145,16 +145,100 @@ public class BST_01 {
         preorder(root.right);
     }
 
-
-    public static Node createBST(int arr6[], int st, int end){
-        if(st > end){
+    public static Node createBST(ArrayList<Integer> list, int st, int end) {
+        if (st > end) {
             return null;
         }
         int mid = st + (end - st) / 2;
-        Node root = new Node(arr6[mid]);
-        root.left = createBST(arr6, st, mid - 1);
-        root.right = createBST(arr6, mid + 1, end);
+        Node root = new Node(list.get(mid));
+        root.left = createBST(list, st, mid - 1);
+        root.right = createBST(list, mid + 1, end);
         return root;
+    }
+
+    public static void inOrderInArrayList(Node root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inOrderInArrayList(root.left, list);
+        list.add(root.data);
+        inOrderInArrayList(root.right, list);
+    }
+
+    public static Node bstToBalancedBST(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrderInArrayList(root, list);
+        Node node = createBST(list, 0, list.size() - 1);
+        return node;
+    }
+
+    static class Info{
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+        public Info(boolean isBST, int size, int min, int max){
+            this.size = size;
+            this.max = max;
+            this.isBST = isBST;
+            this.min = min;
+
+        }
+    }
+    public static int maxBST = 0;
+    public static Info largestBST(Node root){
+        if(root == null){
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        Info left = largestBST(root.left);
+        Info right = largestBST(root.right);
+
+        int size = left.size + right.size + 1;
+        int min = Math.min(root.data, Math.min(left.min, right.min));
+        int max = Math.max(root.data, Math.max(left.max, right.max));
+
+        if(root.data <= left.max || root.data >= right.min){
+            return new Info(false, size, min, max);
+        }
+        if(left.isBST && right.isBST){
+            maxBST = Math.max(maxBST, size);
+            return new Info(true, size, min, max);
+        }
+        return new Info(false, size, min, max);
+    }
+
+    public static Node mergeBSTs(Node root1, Node root2){
+        ArrayList <Integer> list1 = new ArrayList<>();
+        ArrayList <Integer> list2 = new ArrayList<>();
+
+        inOrderInArrayList(root1, list1);
+        inOrderInArrayList(root2, list2);
+
+        ArrayList <Integer> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i < list1.size() && j < list2.size()){
+            if(list1.get(i) <= list2.get(j)){
+                list.add(list1.get(i));
+                i++;
+            }
+            else{
+                list.add(list2.get(j));
+                j++;
+            }
+        }
+        while(i < list1.size()){
+            list.add(list1.get(i));
+            i++;
+        }
+        while(j < list2.size()){
+            list.add(list2.get(j));
+            j++;
+        }
+
+        Node finalRoot = createBST(list, 0, list.size() - 1);
+        return finalRoot;
     }
 
     public static void main(String[] args) {
@@ -225,11 +309,67 @@ public class BST_01 {
         preorder(root5);
         System.out.println();
 
+        // ArrayList <Integer> list1 = { 3, 5, 6, 8, 10, 11, 12 };
+        // Node root6 = createBST(list1, 0, list1.size() - 1);
+        // preorder(root6);
+
+        // System.out.println();
+        // System.out.println();
+
+        Node root7 = new Node(8);
+        root7.left = new Node(6);
+        root7.left.left = new Node(5);
+        root7.left.left.left = new Node(3);
+
+        root7.right = new Node(10);
+        root7.right.right = new Node(11);
+        root7.right.right.right = new Node(12);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        preorder(root7);
+        System.out.println();
+        Node balanced = bstToBalancedBST(root7);
+        preorder(balanced);
+        System.out.println();
 
 
-        int arr6[] = {3, 5, 6, 8, 10, 11, 12};
-        inorder(createBST(arr6, 0, arr6.length - 1));
-        
+
+        Node root10 = new Node(50);
+
+        root10.left = new Node(30);
+        root10.left.left = new Node(5);
+        root10.left.right = new Node(20);
+
+        root10.right = new Node(60);
+        root10.right.left = new Node(45);
+        root10.right.right = new Node(70);
+        root10.right.right.left = new Node(65);
+        root10.right.right.right = new Node(80);
+
+        Info info = largestBST(root10);
+        System.out.println("Largest BST size : " + maxBST);
+
+
+        System.out.println();
+        System.out.println();
+
+        Node bst1 = new Node(2);
+        bst1.left = new Node(1);
+        bst1.right = new Node(4);
+        preorder(bst1);
+        System.out.println();
+
+        Node bst2 = new Node(9);
+        bst2.left = new Node(3);
+        bst2.right = new Node(12);
+        preorder(bst2);
+        System.out.println();
+
+        Node bst3 = mergeBSTs(bst1, bst2);
+        preorder(bst3);
+        System.out.println();
 
     }
 }
